6f1de4f3eed450a3b9cc26ffea72d09fc876d922
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2017-12-05 13:29:05 +0100

    Fixed the model classes and databases, added automatic database erase and some tests

--- app.py
+++ app.py
@@ -6,42 +6,24 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test.db'
 db = SQLAlchemy(app)
 
-#authors = db.Table('authors',
-#    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-#    db.Column('paper_id', db.Integer, db.ForeignKey('paper.id'))
-#)
-
-class Authors(db.Model):
-    __tablename__ = 'authors'
-    id = db.Column(db.Integer, primary_key=True)
-
-    paper_id = db.Column(db.Integer, db.ForeignKey('paper.id'), primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-
-    paper = db.relationship("Paper", back_populates="authors")
-    author = db.relationship("User", back_populates="papers")
+authors = db.Table('authors',
+   db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+   db.Column('paper_id', db.Integer, db.ForeignKey('paper.id'), primary_key=True)
+)
 
 class Score(db.Model):
-    __tablename__ = 'score'
-    id = db.Column(db.Integer, primary_key=True)
-
+    __tablename__ = "score"
     paper_id = db.Column(db.Integer, db.ForeignKey('paper.id'), primary_key=True)
-    reviewer_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     rating = db.Column(db.Integer)
 
-    paper = db.relationship("Paper", back_populates="reviewers")
-    reviewer = db.relationship("User", back_populates="rated_papers")
-
+    reviewer = relationship("User" ,backref="paper_assocs")
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
     hashed_password = db.Column(db.String(120), nullable=False)
-
-    rated_papers = db.relationship("Score", back_populates="reviewer")
-    papers = db.relationship("Authors", back_populates="author")
-    #papers = db.relationship("Paper", secondary="authors", back_populates="authors")
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -54,21 +36,45 @@ class Paper(db.Model):
     title = db.Column(db.String(120), nullable=False)
     abstract = db.Column(db.Text(500), nullable=False)
 
-    reviewers = db.relationship("Score", back_populates="paper")
-    authors = db.relationship("Authors", back_populates="paper")
-    #authors = db.relationship("User", secondary="authors", back_populates="papers")
+    scores = db.relationship("Score", backref="paper")
+    authors = db.relationship('User', secondary=authors, lazy='subquery',
+        backref=db.backref('papers', lazy=True))
 
     def __repr__(self):
         return '<Paper %r>' % self.title
 
-#user1 = User(email="test", hashed_password="test")
-#paper1 = Paper(status="vergeben", title="test", abstract="cool")
-#paper2 = Paper(status="vergebasdasden", title="test2", abstract="cool2")
-#db.session.add(user1)
-#db.session.add(paper1)
-#db.session.add(paper2)
-#db.session.commit()
-user1 = User.query.first
-paper1 = Paper.query.first
-#paper1.author.append(user1)
-print(paper1.title)
+db.drop_all()
+db.create_all()
+
+user1 = User(email="testmail@web.de", hashed_password="test")
+paper1 = Paper(status="vergeben", title="testtitle", abstract="cool")
+paper2 = Paper(status="vergebasdasden", title="testtitle2", abstract="cool2")
+
+db.session.add(user1)
+db.session.add(paper1)
+db.session.add(paper2)
+db.session.commit()
+
+foundUser = User.query.first()
+foundPaper = Paper.query.first()
+
+foundPaper.authors.append(foundUser)
+db.session.add(foundPaper)
+db.session.commit()
+
+foundUser = User.query.first()
+foundPaper = Paper.query.first()
+
+score = Score(rating=5)
+score.reviewer = foundUser
+score.user_id = foundUser.id
+score.paper_id = foundPaper.id
+foundPaper.scores.append(score)
+
+db.session.add(score)
+db.session.commit()
+
+foundUser = User.query.first()
+foundPaper = Paper.query.first()
+print(foundPaper.scores[0].user_id == foundUser.id)
+print(foundPaper.scores[0].paper_id == foundPaper.id)
