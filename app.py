4a48c709cca75c72b051d305892af04b8ebde06a
jberauer <julia.berauer@cross4channel.de>
2017-12-07 17:56:54 +0100

    added registration and stuff

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test.db'
 db = SQLAlchemy(app)
 
 dummy = Dummy()
-#app.current_user = None
+# app.current_user = None
 app.current_user = User.query.first()
 
 
@@ -17,9 +17,10 @@ def index():
     if app.current_user == None:
         return render_template('login.html')
     else:
-        papers = Paper.query.filter_by(Paper.authors.any(id=app.current_user.id))
+        papers = Paper.query.filter(Paper.authors.any(id=app.current_user.id)).all()
         print(papers)
         return render_template('index.html', current_user=app.current_user, papers=papers)
+
 
 @app.route('/login')
 def login():
@@ -28,33 +29,61 @@ def login():
     else:
         return render_template('index.html', current_user=app.current_user)
 
-@app.route('/logging_in',methods = ['POST', 'GET'])
-def logging_in():
-   if request.method == 'POST':
-      result = request.form
-      print(result)
-      user = User.query.filter_by(email=result.get('email')).first()
-      if user and user.hashed_password == result.get('password'):
-          app.current_user = user
-          return render_template('index.html', current_user=app.current_user)
-      else:
-          return render_template('login.html', message="Login failed")
+@app.route('/logout')
+def logout():
+    app.current_user = None
+    return render_template('login.html')
 
+@app.route('/logging_in', methods=['POST', 'GET'])
+def logging_in():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        user = User.query.filter_by(email=result.get('email')).first()
+        if user and user.hashed_password == result.get('password'):
+            app.current_user = user
+            return render_template('index.html', current_user=app.current_user)
+        else:
+            return render_template('login.html', message="Login failed")
+
+@app.route('/registration')
+def registration():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        new_user = User(email=result.get('email'), hashed_password=result.get('password'))
+        print(new_user)
+        db.session.commit()
+        app.current_user = new_user
+        return render_template('index.html', current_user=app.current_user)
 
 @app.route('/paper_submission')
-def hello_name():
-   return render_template('paper_submission.html')
+def paper_submission():
+    return render_template('paper_submission.html')
 
-@app.route('/submit_paper',methods = ['POST', 'GET'])
+
+@app.route('/submit_paper', methods=['POST', 'GET'])
 def submit_paper():
-   if request.method == 'POST':
-      result = request.form
-      print(result)
-      new_paper = Paper(status="unreviewed", title=result.get('title'), abstract=result.get('abstract'))
-      print(new_paper)
-      db.session.add(new_paper)
-      db.session.commit()
-      return render_template('index.html', current_user=app.current_user)
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        new_paper = Paper(status="under review", title=result.get('title'), abstract=result.get('abstract'))
+        new_paper.authors.append(app.current_user)
+        print(new_paper)
+        db.session.commit()
+        papers = Paper.query.filter(Paper.authors.any(id=app.current_user.id)).all()
+        # TODO Add other authors too
+        return render_template('index.html', current_user=app.current_user, papers=papers)
+
+@app.route('/set_roles')
+def set_roles():
+    return render_template('set_roles.html')
+
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
